77f6b2348833aca515ecc4a27ffad30f2e22b3b2
Andrew Subotin <subotinandrey5@gmail.com>
2023-01-05 11:53:37 +0200

    display doesn`t disappear immediately

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import pygame
 
 
 pygame.init()
-dis = pygame.display.set_mode((400, 300))
-pygame.display.update()
+dis = pygame.display
+dis.set_mode((400, 300))
+dis.set_caption("Snake game")
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
 pygame.quit()
 quit()
